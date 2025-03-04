72fc723a15efe38c95e95e3fc8d942b05f843cdf
Alvaro Sarria <alvaro.sarria.rico@gmail.com>
2025-03-04 17:31:32 +0100

    starting navbar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,17 @@
+import "./NavBar.module.css"
+
+const NavBar = () => {
+    return (
+        <nav className="portfolio-navbar">
+            <ul>
+                <li>About</li>
+                <li>Experience</li>
+                <li>Portfolio</li>
+                <li>Contact</li>
+                <li>Icons</li>
+            </ul>
+        </nav>
+    );
+};
+
+export default NavBar;
